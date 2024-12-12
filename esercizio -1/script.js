fac58239fda196ca48ebc4e98843e19f56d854a5
Matteo Cipolla <mati_teo@live.it>
2024-12-12 15:23:43 +0100

    feat:add 1-exercise

--- esercizio -1/script.js
+++ esercizio -1/script.js
@@ -2,3 +2,45 @@
 //Creare un array di oggetti:
 //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore.
+
+const bikes = [
+    {
+        marca: "bianchi",
+        peso: "8",
+    },
+
+    {
+        marca: "cinelli",
+        peso: "8,6",
+    },
+
+    {
+        marca: "santa cruz",
+        peso: "3,4",
+    },
+
+    {
+        marca: "focus",
+        peso: "7,7",
+    },
+
+    {
+        marca: "scott",
+        peso: "6",
+    },
+
+    {
+        marca: "graziella",
+        peso: "2",
+    },
+];
+
+let lightweightbike = bikes[0];
+
+for (let i = 1; i < bikes.length; i++) {
+    if (bikes[i].peso < lightweightbike.peso) {
+        lightweightbike = bikes[i];
+    }
+}
+
+console.log("La bici più leggera è:", lightweightbike.marca, "con un peso di", lightweightbike.peso, "kg");
